2519ef6a41a38f62b02dcce0cbca363c6ffde918
Oscar Diaz <o-pipe@hotmail.com>
2018-07-05 15:53:34 -0500

    Inslatacion paquete para manejar Zip

--- config/app.php
+++ config/app.php
@@ -177,6 +177,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+        * Package Install
+        */
+        Chumper\Zipper\ZipperServiceProvider::class
     ],
 
     /*
@@ -225,6 +229,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Zipper' => Chumper\Zipper\Zipper::class,
 
     ],
 
