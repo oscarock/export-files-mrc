1d4e9537c6df37b9d8bea4c95309aa213f15d76e
Oscar Diaz <o-pipe@hotmail.com>
2018-07-06 02:43:40 -0500

    Ajsute funcion para el export

--- app/Http/Controllers/ExportFileController.php
+++ app/Http/Controllers/ExportFileController.php
@@ -11,18 +11,21 @@ use Illuminate\Http\Request;
 class ExportFileController extends Controller
 {
     public function createFiles(){
-      /*$issues = Issue::where('tenant_id', '=' ,6)->get();
+      $issues = Issue::where('tenant_id', '=' ,6)->get();
       foreach ($issues as $issue) {
         $file_content = '"'.$issue->name."\r\n"
                            .$issue->description."\r\n"
                            .$issue->publication_date.'"';
         Storage::put($issue->tenant_id.'/'.$issue->slug.'.mrc', $file_content);
-      }*/
-      //var_dump($issue->tenant_id);
-      //var_dump(storage_path('app/'.$issue->tenant_id));
-      $files = glob(storage_path());
-      Zipper::zip('storage/MARC-export.zip')->add($files);
-      //Zipper::make('storage/MARC-export.zip')->folder($issue->tenant_id)->add('storage/app/'.$issue->tenant_id.'/*');
+      }
+       
+      //var_dump(file_exists(storage_path('app/'.$issue->tenant_id)));
 
+      $files = glob(storage_path('app/'.$issue->tenant_id));
+      //Zipper::zip('storage/MARC-export.zip')->folder($issue->tenant_id)->add($files);
+      Zipper::zip(storage_path('app/MARC-export.zip'))->folder($issue->tenant_id)->add($files);
+      //return response()->download('storage/MARC-export.zip');
+      //Storage::deleteDirectory($issue->tenant_id);
+      //var_dump(Storage::deleteDirectory($issue->tenant_id));
     }
 }
