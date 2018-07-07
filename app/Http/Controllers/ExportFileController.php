afbd9f33c9e294f327daecb9667f4c920f4d2d5e
Oscar Diaz <o-pipe@hotmail.com>
2018-07-07 03:43:41 -0500

    Ajuste de la funcion crear archivos

--- app/Http/Controllers/ExportFileController.php
+++ app/Http/Controllers/ExportFileController.php
@@ -11,7 +11,7 @@ use Illuminate\Http\Request;
 class ExportFileController extends Controller
 {
     public function createFiles(){
-      $issues = Issue::where('tenant_id', '=' ,6)->get();
+      $issues = Issue::where('tenant_id', '=' , 6)->get();
       foreach ($issues as $issue) {
         $file_content = '"'.$issue->name."\r\n"
                            .$issue->description."\r\n"
@@ -19,13 +19,10 @@ class ExportFileController extends Controller
         Storage::put($issue->tenant_id.'/'.$issue->slug.'.mrc', $file_content);
       }
        
-      //var_dump(file_exists(storage_path('app/'.$issue->tenant_id)));
-
       $files = glob(storage_path('app/'.$issue->tenant_id));
-      //Zipper::zip('storage/MARC-export.zip')->folder($issue->tenant_id)->add($files);
       Zipper::zip(storage_path('app/MARC-export.zip'))->folder($issue->tenant_id)->add($files);
-      //return response()->download('storage/MARC-export.zip');
-      //Storage::deleteDirectory($issue->tenant_id);
-      //var_dump(Storage::deleteDirectory($issue->tenant_id));
+      return response()->json([
+        'download_zip' =>  storage_path('app/MARC-export.zip')
+      ]);
     }
 }
