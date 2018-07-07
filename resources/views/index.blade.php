d3979394e6b9b56dcb186628a154eefc4b96d4c0
Oscar Diaz <o-pipe@hotmail.com>
2018-07-07 03:44:32 -0500

    Crea front para descargar archivos

---
+++ resources/views/index.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta name="viewport" content="width=device-width" />
+    <title>Exportar Archivos .MCR</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" >
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js"></script>
+    <script>
+        $(function () {
+            $("#docstatus").click(function () {
+
+                $("#result").empty();
+                $("#prg .progress-bar").css("width", "50%").addClass("progress-bar-striped active").html("50%");
+                $.ajax({
+                    url: "/createFiles",
+                    success: function (data) {
+                        $("#prg .progress-bar").css({ "width": "100%" }).removeClass("active").html("100%");
+                        $("#result").html(data);
+                        location.href = data.download_zip
+                        console.log(data.download_zip)
+                    }, error: function () {
+                        alert("error");
+                    }
+                });
+
+            });
+        });
+    </script>
+</head>
+<body>
+    <div class="container">
+    	<div class="row">
+    	  <div class="col-md-12">
+    	  	<h2 class="text-center">Exportar Archivos .MCR</h2>
+    	  	<br>
+    	  	<div class="progress" id="prg">
+    	  	    <div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+    	  	</div>
+    	  </div>
+    	</div>
+    	<div class="row">
+    	  <div class="col-md-12">
+    	  	<input type="button" class="btn btn-primary btn-block" value="Exportar" id="docstatus" />
+    	  </div>
+    	</div>
+   </div>
+</body>
+</html>
